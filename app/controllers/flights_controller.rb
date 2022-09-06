7597390bc7dddeb2cdc8c5eae27b21ab24db6222
supersakana <zacwilliamson@icloud.com>
2022-09-06 15:24:53 -0600

    added basic flight search form... work in progress

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,7 @@
 class FlightsController < ApplicationController
-    def index
-        @flights = Flight.all
-    end
+  def index
+    @departing_options = Flight.all.map { |f| "#{f.departure_airport.code}, #{f.departure_airport.location}" }
+    @arriving_options = Flight.all.map { |f| "#{f.arrival_airport.code}, #{f.arrival_airport.location}" }
+  end
 end
+2
