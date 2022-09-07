cb0de0a0727fef2664414b38566b08bad946ca5a
supersakana <zacwilliamson@icloud.com>
2022-09-07 15:55:36 -0600

    added passenger count to form

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_213832) do
-  create_table "airports", force: :cascade do |t|
-    t.string "code"
-    t.string "location"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.0].define(version: 20_220_907_213_832) do
+  create_table 'airports', force: :cascade do |t|
+    t.string 'code'
+    t.string 'location'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "flights", force: :cascade do |t|
-    t.integer "departure_airport_id"
-    t.integer "arrival_airport_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "duration"
-    t.datetime "date"
-    t.integer "passenger_count"
-    t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
-    t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
+  create_table 'flights', force: :cascade do |t|
+    t.integer 'departure_airport_id'
+    t.integer 'arrival_airport_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'duration'
+    t.datetime 'date'
+    t.integer 'passenger_count'
+    t.index ['arrival_airport_id'], name: 'index_flights_on_arrival_airport_id'
+    t.index ['departure_airport_id'], name: 'index_flights_on_departure_airport_id'
   end
 
-  add_foreign_key "flights", "airports", column: "arrival_airport_id"
-  add_foreign_key "flights", "airports", column: "departure_airport_id"
+  add_foreign_key 'flights', 'airports', column: 'arrival_airport_id'
+  add_foreign_key 'flights', 'airports', column: 'departure_airport_id'
 end
