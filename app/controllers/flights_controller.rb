ba45ed65fa025b160359de287f7dc68ab13bf033
supersakana <zacwilliamson@icloud.com>
2022-09-08 09:32:53 -0600

    when user submits data, departure and arrival flight ids are collected in the parameters

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,10 @@
 class FlightsController < ApplicationController
   def index
-    @departing_options = Flight.all.map { |f| "#{f.departure_airport.code}, #{f.departure_airport.location}" }
-    @arriving_options = Flight.all.map { |f| "#{f.arrival_airport.code}, #{f.arrival_airport.location}" }
+    @departing_options = Flight.all.map do |f|
+      ["#{f.departure_airport.code}, #{f.departure_airport.location}", f.departure_airport.id]
+    end
+    @arriving_options = Flight.all.map do |f|
+      ["#{f.arrival_airport.code}, #{f.arrival_airport.location}", f.arrival_airport.id]
+    end
   end
 end
-2
