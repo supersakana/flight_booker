d6d40e73b26756dd7c49d13a44731d5403040d2e
supersakana <zacwilliamson@icloud.com>
2022-09-08 12:53:53 -0600

    cleaned up flights controller a bit (moved interpolation to Flight class

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,17 @@
 class FlightsController < ApplicationController
   def index
     @departing_options = Flight.all.map do |f|
-      ["#{f.departure_airport.code}, #{f.departure_airport.location}", f.departure_airport.id]
+      [f.from, f.departure_airport.id]
     end
     @arriving_options = Flight.all.map do |f|
-      ["#{f.arrival_airport.code}, #{f.arrival_airport.location}", f.arrival_airport.id]
+      [f.to, f.arrival_airport.id]
     end
+    @search_results = Flight.search(search_params)
+  end
+
+  private
+
+  def search_params
+    params.permit(:departure_airport_id, :arrival_airport_id)
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,17 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+
+  def from
+    "#{departure_airport.code}, #{departure_airport.location}"
+  end
+
+  def to
+    "#{arrival_airport.code}, #{arrival_airport.location}"
+  end
+
+  def self.search(search_params)
+    where('departure_airport_id == ?', search_params[:departure_airport_id]) &&
+      where('arrival_airport_id == ?', search_params[:arrival_airport_id])
+  end
 end
