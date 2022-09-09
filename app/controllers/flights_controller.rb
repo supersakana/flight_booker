88065c4c70c8d52edeee72fd1cc175a22daf2f24
supersakana <zacwilliamson@icloud.com>
2022-09-09 11:44:24 -0600

    added passenger count hidden field

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,6 +3,7 @@ class FlightsController < ApplicationController
     @departing_options = Flight.all.map { |f| [f.from, f.departure_airport.id] }
     @arriving_options = Flight.all.map { |f| [f.to, f.arrival_airport.id] }
     @search_results = Flight.search(search_params)
+    @passengers = search_params[:passenger_count]
   end
 
   private
