132cc7d077f969f22a1893088f38845e6eedc684
supersakana <zacwilliamson@icloud.com>
2022-09-10 10:41:43 -0600

    finished basic form layout for booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,11 @@
 class BookingsController < ApplicationController
-  def new; end
+  def new
+    @booking = Booking.new
+    @flight = Flight.find(params[:flight_id])
+    @passengers = params[:passenger_count].to_i
+  end
+
+  # def create
+
+  # end
 end
