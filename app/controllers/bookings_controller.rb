11572da22862c63d9b3217b836b1b39c2315961b
supersakana <zacwilliamson@icloud.com>
2022-09-11 16:45:09 -0600

    when booking form is submitted, redirects to booking show page with flight/passenger details (rough draft)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,6 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: %i[show edit update destroy]
+
   def new
     @booking = Booking.new
     @flight = Flight.find(params[:flight_id])
@@ -6,10 +8,26 @@ class BookingsController < ApplicationController
     @p_num.times { @booking.passengers.build }
   end
 
+  def create
+    @booking = Booking.new(booking_params)
 
-  # private
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_entity
+      render json: @booking.errors, status: :unprocessable_entity
+    end
+  end
 
-  # def booking_params
-  #   params.require(:booking).permit(:passenger_id, :flight_id)
-  # end
+  def show; end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: %i[id name email])
+  end
 end
