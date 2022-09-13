cfc490b7a410f5e386cf72f45bd880401f3220f3
supersakana <zacwilliamson@icloud.com>
2022-09-13 16:38:52 -0600

    finished styling index page

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,6 +1,12 @@
 class Airport < ApplicationRecord
-  has_many :departing_flights, foreign_key: 'departure_airport_id', class_name: 'Flight', dependent: :destroy
-  has_many :arriving_flights, foreign_key: 'arrival_airport_id', class_name: 'Flight', dependent: :destroy
+  has_many :departing_flights,
+           foreign_key: 'departure_airport_id',
+           class_name: 'Flight',
+           dependent: :destroy
+  has_many :arriving_flights,
+           foreign_key: 'arrival_airport_id',
+           class_name: 'Flight',
+           dependent: :destroy
 
   def info
     "#{code} #{location}"
