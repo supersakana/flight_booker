e417e2533fe92a68dc0f2f710e28af981ef0e510
supersakana <zacwilliamson@icloud.com>
2022-09-18 12:10:39 -0600

    fixed search button responsiveness

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -7,6 +7,11 @@ class FlightsController < ApplicationController
 
   private
 
+  # def search_flight
+  #   if search_params[departure_airport_id] == search_params[arrival_airport_id]
+  #     flash
+  # end
+
   def search_params
     params.permit(:departure_airport_id, :arrival_airport_id, :passenger_count, :date, :commit)
   end
