97292b7977396a52f00a6d209434ce7ca9990800
supersakana <zacwilliamson@icloud.com>
2022-09-20 16:29:48 -0600

    added 2 more search flight feature tests

--- spec/features/search_flight_spec.rb
+++ spec/features/search_flight_spec.rb
@@ -14,9 +14,29 @@ RSpec.feature 'SearchFlights', type: :feature do
     fill_in('date', with: Date.tomorrow)
     click_on 'Search'
     formatted_date = Date.tomorrow.strftime('%B %d %Y')
-    sleep(3)
-    
+
     expect(page).to have_content("#{formatted_date} | ATL Atlanta, Georgia - ORF Norfolk, Virginia")
     expect(page).to have_content('08:00 AM')
+  end
+
+  scenario 'no date is selected' do
+    visit '/'
+    select('ATL Atlanta, Georgia', from: 'departure_airport_id')
+    select('ORF Norfolk, Virginia', from: 'arrival_airport_id')
+    select('2', from: 'passenger_count')
+    click_on 'Search'
+
+    expect(page).to have_content('Search for flight...')
+  end
+
+  scenario 'departure and arrival airport are the same' do
+    visit '/'
+    select('ORF Norfolk, Virginia', from: 'departure_airport_id')
+    select('ORF Norfolk, Virginia', from: 'arrival_airport_id')
+    select('2', from: 'passenger_count')
+    fill_in('date', with: Date.tomorrow)
+    click_on 'Search'
+
+    expect(page).to have_content('Search for flight...')
   end
 end
