e65cc0573b92d3bee281210d5247f37840e17ef2
supersakana <zacwilliamson@icloud.com>
2022-10-03 19:08:18 -0600

    added basic mailer views for passengers

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,7 @@
 class PassengerMailer < ApplicationMailer
+  def welcome_email
+    @passenger = params[:passenger]
+    # @url = 'http://example.com/login'
+    mail(to: @passenger.email, subject: 'Your flight is booked!')
+  end
 end
